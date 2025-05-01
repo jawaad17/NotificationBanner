5c387adf0073c3dbba31408263eaa54fe48d82b9
Jaydee <jaydi1288@gmail.com>
2025-05-01 13:01:39 +0500

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -1,22 +1,26 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "NotificationBannerSwift",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13),
+        .macCatalyst(.v13)
     ],
     products: [
-        .library(name: "NotificationBannerSwift", targets: ["NotificationBannerSwift"])
+        .library(
+            name: "NotificationBannerSwift",
+            targets: ["NotificationBannerSwift"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1"),
-        .package(url: "https://github.com/cbpowell/MarqueeLabel", from: "4.5.0")
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .exact("5.7.1")),
+        .package(url: "https://github.com/cbpowell/MarqueeLabel.git", .exact("4.5.0"))
     ],
     targets: [
         .target(
             name: "NotificationBannerSwift",
-            dependencies: [.byName(name: "MarqueeLabel"), .byName(name: "SnapKit")],
+            dependencies: ["SnapKit", "MarqueeLabel"],
             path: "NotificationBanner/Classes"
         )
     ],
